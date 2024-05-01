a356f47adba16669957ac57fe337de055138b2b6
kpatel0170 <kartikpatel0170@gmail.com>
2024-04-30 18:50:38 -0600

    Update app and database names

--- src/db/connections.js
+++ src/db/connections.js
@@ -15,20 +15,20 @@ module.exports = async () => {
     // GLOBAL Connections
     const CONNECTION_IN_AGTECH = await connectionFactory.getConnection(
       "GLOBAL",
-      config.MONGODB.GLOBAL.DATABASE.agriGen
+      config.MONGODB.GLOBAL.DATABASE.upCrops
     );
 
     const mongooseConnections = {
       GLOBAL: {
-        agriGen: CONNECTION_IN_AGTECH,
-      },
+        upCrops: CONNECTION_IN_AGTECH
+      }
     };
 
     /* All the (mongoose) models to be defined here */
     models = {
       GLOBAL: {
         ADMIN: require("../schema/admin/admin")(CONNECTION_IN_AGTECH),
-        LOG: require("../schema/log/log")(mongooseConnections.GLOBAL.agriGen),
+        LOG: require("../schema/log/log")(mongooseConnections.GLOBAL.upCrops),
         ROLE: require("../schema/role/role")(CONNECTION_IN_AGTECH),
         PRODUCT: require("../schema/product/product")(CONNECTION_IN_AGTECH),
         CART: require("../schema/cart/cart")(CONNECTION_IN_AGTECH),
@@ -36,8 +36,8 @@ module.exports = async () => {
           CONNECTION_IN_AGTECH
         ),
         PAYMENT: require("../schema/payment/payment")(CONNECTION_IN_AGTECH),
-        PLANS: require("../schema/plans/plans")(CONNECTION_IN_AGTECH),
-      },
+        PLANS: require("../schema/plans/plans")(CONNECTION_IN_AGTECH)
+      }
     };
 
     return models;
